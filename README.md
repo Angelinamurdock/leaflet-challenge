e7fc4b6f1262adfe38ae12e1b14879606cea6a3d
Angelinamurdock <angelinamurdock@MacBook-Air.lan>
2025-04-05 23:26:30 -0600

    added legend to the logic.js file and styling for the legend to the style.css file

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-# leaflet-challenge
+# Leaflet Earthquake Visualization
+**Creator**: Angelina Murdock  
+**Date**: April 2025
+
+## Table of contents
+- [Overview](#overview)
+- [Deployment](#deployment)
+- [Features](#features)
+- [Methodology](#methodology)
+- [Resources](#resources)
+
+## Overview
+A interactive project exploring the biodiversity of belly buttons using **JavaScript**. This app allows users to visualize different species of bacteria found in the belly buttons of various test subjects, displayed using data visualizations and interactive charts.
 
 all earthquakes in the last 30 days
+
+### Dashboard Example:
+
+![Dashboard Example](dashboard_example/dashboard_image.png)
+
+## Deployment
+This app is deployed on **GitHub Pages**: [Click here to deploy the app](https://angelinamurdock.github.io/belly-button-challenge/)
+
+## Features
+`samples.json`: Contains demographic information and sample data for all of the test subjects, including bacteria culture data.
+
+`app.js`:
+- Implements a bar chart to visualize data of the top 10 bacteria cultures found in each test subject.
+- Implements a bubble chart for visualizing data of the number of bacteria cultures per sample by OTU ID.
+- Loop through metadata to get the demographic info for each individual and populates it into a panel.
+
+`index.html`: The main HTML file that connects all components of the app.
+
+## Methodology
+### Data Processing and Visualization
+**JavaScript Functions**: The app relies on JavaScript functions to handle data extraction, dynamic visualization updates and interactivity. Key functions include:
+- Loading and parsing JSON data from `samples.json`.
+- Uploading charts and panels based on user interaction.
+
+**Plotly Data Visualizations**: The app uses Plotly to create interactive bar and bubble charts. Plotly allows users to interact with the data points, making the visualizations both informative and engaging.
+
+## Resources
+* **USGS GeoJSON Feed:** http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php 
+* **DU Bootcamp Module 15:** Utilized challenge files and class materials from the bootcamp.
+* **ChatGPT:** Assisted with code explanations and debugging.
+* **Determining Earthquake Depths:** https://www.usgs.gov/programs/earthquake-hazards/determining-depth-earthquake 
--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,4 +1,5 @@
-// Create the tile layers that will be the background of our map.
+// Create the tile layers that will be the background of our map and the map object.
+
 // Create the 'topo' tile layer.
 let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
@@ -9,13 +10,6 @@ let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-// Create the map object with center and zoom options.
-let myMap = L.map("map", {
-  center: [40.73, -74.0059],
-  zoom: 12,
-  layers: [topo]  // add the 'topo' base tile layer to the map.
-});
-
 
 // Create the layer groups, base maps, and overlays for our two sets of data, earthquakes and tectonic_plates.
 // Add a control to the map that will allow the user to change which layers are visible.
@@ -23,6 +17,13 @@ let myMap = L.map("map", {
 // Create the layer groups
 let earthquakes = L.layerGroup();
 let techtonic_plates = L.layerGroup();
+
+// Create the map object with center and zoom options.
+let myMap = L.map("map", {
+  center: [37.75, -121.05],
+  zoom: 8,
+  layers: [street, earthquakes]  // add the 'street' base tile layer to the map.
+});
 
 // Create a baseMaps object
 let baseMaps = {
@@ -44,39 +45,60 @@ L.control.layers(baseMaps, overlayMaps, {
 
 // Make a request that retrieves the earthquake geoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").then(function (data) {
-
+  
   // This function returns the style data for each of the earthquakes we plot on
   // the map. Pass the magnitude and depth of the earthquake into two separate functions
   // to calculate the color and radius.
   function styleInfo(feature) {
-
+    return {
+      opacity: 1,
+      fillOpacity: 0.6,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "black",
+      radius: getRadius(feature.properties.mag),
+      weight: 0.5
+    };
   }
 
   // This function determines the color of the marker based on the depth of the earthquake.
   function getColor(depth) {
-
+    if (depth <= 70) {
+      return "#98ee00"; // Green for shallow earthquakes
+    }
+    else if (depth <= 150) {
+      return "#d4ee00"; // Yellow 
+    }
+    else if (depth <= 300) {
+      return "#eecc00"; // Orange for moderate earthquakes
+    }
+    else if (depth <= 500) {
+      return "#ea822c"; // Red-orange
+    }
+    else {
+      return "#ea2c2c";  // Red for deep earthquakes
+    }
   }
 
   // This function determines the radius of the earthquake marker based on its magnitude.
   function getRadius(magnitude) {
-
+    return magnitude * 8;
   }
 
   // Add a GeoJSON layer to the map once the file is loaded.
   L.geoJson(data, {
     // Turn each feature into a circleMarker on the map.
     pointToLayer: function (feature, latlng) {
-
+      return L.circleMarker(latlng, styleInfo(feature));
     },
     // Set the style for each circleMarker using our styleInfo function.
     style: styleInfo,
     // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
     onEachFeature: function (feature, layer) {
-
+      layer.bindPopup(`<h3>${feature.properties.mag}</h3><hr><p>${feature.properties.place}</p>`);
     }
-  // OPTIONAL: Step 2
+
   // Add the data to the earthquake layer instead of directly to the map.
-  }).addTo(myMap);
+  }).addTo(earthquakes);
 
   // Create a legend control object.
   let legend = L.control({
@@ -88,16 +110,28 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geo
     let div = L.DomUtil.create("div", "info legend");
 
     // Initialize depth intervals and colors for the legend
+    let limits = [0,70,150,300,500];
+    let colors = ["#98ee00", "#d4ee00", "#eecc00", "#ea822c", "#ea2c2c"];
+    let labels = ['<strong>Earthquake Depth</strong>'];
+
 
 
     // Loop through our depth intervals to generate a label with a colored square for each interval.
+    for (let i = 0; i < limits.length; i++) {
+      labels.push(
+        '<i style="background:' + colors[i] + '"></i> ' +
+        limits[i] + (limits[i + 1] ? '&ndash;' + limits[i + 1] + ' km' : '+ km')
+      );
+    }
 
+    // Join the labels into one string and add it to the div
+    div.innerHTML = labels.join("<br>");
 
     return div;
   };
 
   // Finally, add the legend to the map.
-
+  legend.addTo(myMap);
 
   // OPTIONAL: Step 2
   // Make a request to get our Tectonic Plate geoJSON data.
